2f8d334aa69c919e21c768bec80e17ca813de2f4
RicardoVelazco <31299653+RicardoVelazco@users.noreply.github.com>
2017-11-10 13:59:02 -0600

    Add files via upload

---
+++ velocidad.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+  double d=0;
+  double t=0;
+  double v=0;
+  cout<<"Dama la distancia" <<endl;
+  cin>>d; 
+  cout<<"Dame el timepo" <<endl;
+  cin>>t;
+  v=d/t;
+  cout<<"Velocidad es igual a" <<v<<endl;
+  return 0;
+}
