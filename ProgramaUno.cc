361dfb81b77a27f21774adc1398d9f58b7c7f742
RicardoVelazco <31299653+RicardoVelazco@users.noreply.github.com>
2017-11-10 13:24:47 -0600

    Add files via upload

--- ProgramaUno.cc
+++ ProgramaUno.cc
@@ -1,9 +1,11 @@
+//26 es el valor que resulta 
+//Ricardo Alfonso Velazco Castillo
+//Nacì un 26 de Octubre 
 #include <iostream>
 using namespace std;
 
 int isPrime(int X){
   int flag=0; 
- 
   for(int i=2; i<= (X/2) ; i++){
     if(X%i==0){
       flag=1;
@@ -11,22 +13,17 @@ int isPrime(int X){
     }
   }   
   return flag;
-
 }
-
 int main (){
   int S=12;
   int p=0;
   int flag=0;
-
-  cout << "Dame un número entero" << endl;
+ cout << "Dame un número entero" << endl;
   cin >> p;
-
  while (S!=0){ 
    cout << "ciclo S!=0, S " << S << ",p " << p << endl;
    while(p<S){
-       cout << "ciclo p<S, S " < S << ",p " << p << endl;
-
+       cout << "ciclo p<S, S " << S << ",p " << p << endl;
     while(isPrime(p)==1){
       cout << "ciclo isPrime, S " << S << ",p " << p << endl;
       cout << p << " no es primo  " << endl;
@@ -35,16 +32,13 @@ int main (){
       p=p+2;
       cout << "final ciclo isPrime, S " << S << ",p " << p << endl;
     }  
-    
     S=S-p;
-    p=p+2
+    p=p+2;
     cout << "final ciclo p<S, S " << S << ",p " << p << endl;
    }
-
   S=S-1;
-  cout < "final ciclo S!=0, S " << S << ",p " << p << endl;
+  cout << "final ciclo S!=0, S " << S << ",p " << p << endl;
  }
-  
-  cout << " el valor de P es " << p << endl;
+   cout << " el valor de P es " << p << endl;
   return 0;
 }
